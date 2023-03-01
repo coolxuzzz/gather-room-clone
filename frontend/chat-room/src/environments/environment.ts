5281d5aefa2a3da951065ef8c9c62cd210571cf5
Xu Zheng <barry.zheng@mail.utoronto.ca>
2023-03-01 13:32:03 -0500

    connect to firebase

---
+++ frontend/chat-room/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBE7Kzg1ueicv9JNrvvUU9Jj1jnlqbsJl8',
+    authDomain: 'chat-room-2982b.firebaseapp.com',
+    projectId: 'chat-room-2982b',
+    storageBucket: 'chat-room-2982b.appspot.com',
+    messagingSenderId: '350052718241',
+    appId: '1:350052718241:web:cf1d4c6d4cb3635dfe42e0',
+  },
+};
